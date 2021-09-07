713c766ff77345af6e81b2ac5136eac4d2c09d36
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-07 11:42:48 +0000

    abc

---
+++ booleanInPython&conditionalStatementsInPython.py
@@ -0,0 +1,16 @@
+is_Male=True
+is_Wearing_Glasses= True
+charName= "Mike"
+
+if(is_Male==True and is_Wearing_Glasses==True):
+  print("Mike is a male who wears glasses.")
+elif(is_Male==False and is_Wearing_Glasses==True):
+  print("Mike is not a male and he is wearing glasses")
+elif(is_Male==true and is_Wearing_Glasses==False):
+  print("Mike is a male who does not wear glasses")
+else: 
+  print("Mike is not a male and he is not wearing glasses")
+
+
+#This is a python program which prints the situations #of being a male and wearing glasses for the #charachter called "Mike". 
+
