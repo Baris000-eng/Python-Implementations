3cbdfea46503ed970f6258e08d31b53a2f2633f2
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-09 20:09:17 +0000

    calculate

---
+++ basicCalculator.py
@@ -0,0 +1,8 @@
+my_number1= input("Please enter the number 1: ")
+my_number2= input("Please enter the number 2: ")
+addition=int(my_number1)+int(my_number2)
+print("The addition of  "+str(my_number1)+" and "+str(my_number2)+" is "+str(addition))
+
+
+
+
