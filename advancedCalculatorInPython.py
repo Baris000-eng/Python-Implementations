5a6843c78e9cf99d48da0bd9b5958d78f0427247
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-09 21:55:30 +0000

    the implementation of an advanced calculator in python

---
+++ advancedCalculatorInPython.py
@@ -0,0 +1,19 @@
+number1= input("Please enter the first number: ")
+number2= input("Please enter the second number: ")
+operatorType= input("Please enter the operator type: ")#Getting number1, number2, and operatorType from the user.
+
+
+number1= float(number1)#Converting the string version of the number1 to float 
+number2= float(number2) #Converting the string version of the number2 to float
+
+
+if(operatorType=='+'): #Addition case
+  print(number1+number2)
+elif(operatorType=='-'):#Subtraction case
+  print(number1-number2)
+elif(operatorType=='*' or operatorType=='x'):#Multiplication case
+  print(number1*number2)
+elif(operatorType=='/' or operatorType==':'): #Division case
+  print(number1/number2)
+else:
+  print("Invalid operator type.") #Invalid cases
