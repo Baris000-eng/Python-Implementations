51382a0d2006da9368b51cb26cab91ec6d60c0a2
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-09 20:14:11 +0000

    madLibs commit

---
+++ madLibs.py
@@ -0,0 +1,7 @@
+colorOfObject= input("Enter the color of the object: ")
+plural_noun= input("Enter a plural noun: ")
+famous_person=input("Enter a famous person name: ")
+print("Oranges are "+colorOfObject)
+print("I have some "+plural_noun)
+print("My favorite famous person is "+famous_person)
+#This is a python program that prints out some messages depending on the user inputs.
