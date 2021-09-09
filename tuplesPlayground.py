8ccdfba2458f07fd8a51eb38d3f132ac135d5558
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-09 20:44:01 +0000

    tuples

---
+++ tuplesPlayground.py
@@ -0,0 +1,9 @@
+xAndY=(5,6)
+print(xAndY)
+print(xAndY[0])#First element of the tuple called "xAndY".
+print(xAndY[1])#Second element of the tuple called "xAndY".
+listOfTuples=[(2,3),(-1,4),(5,6)]
+print(listOfTuples)#To print a list of tuples
+print(listOfTuples[0])#The first element of the list of tuples
+print(listOfTuples[1])#The second element of the list of tuples
+print(listOfTuples[2])#The third element of the list of tuples
