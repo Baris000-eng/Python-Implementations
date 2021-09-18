0b0ad38f5fb79e76a77915e899f0092e9f0f71cc
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-18 16:05:30 +0000

    RPS

--- ROCKPAPERSCISSORS.py
+++ ROCKPAPERSCISSORS.py
@@ -1,41 +1,43 @@
-choices= ["rock","paper","scissors"]
+import random
+
+
 user1Count=0
 user2Count=0
 winNum=3
 
 while True:
-
-  user1= input("My choice is : ")
+  choices=["rock","paper","scissors"]
+  compChoice=random.choice(choices)
   user2= input("My choice is : ")
   print()
-  if(user1==choices[0] and user2==choices[1]):
+  if(compChoice==choices[0] and user2==choices[1]):
     user2Count+=1
-  elif(user1==choices[0] and user2==choices[2]):
+  elif(compChoice==choices[0] and user2==choices[2]):
     user1Count+=1
-  elif(user1==choices[2] and user2==choices[1]):
+  elif(compChoice==choices[2] and user2==choices[1]):
     user1Count+=1
-  elif(user1==choices[1] and user2==choices[0]):
+  elif(compChoice==choices[1] and user2==choices[0]):
     user1Count+=1
-  elif(user1==choices[2] and user2==choices[0]):
+  elif(compChoice==choices[2] and user2==choices[0]):
    user2Count+=1
-  elif(user1==choices[1] and user2==choices[2]):
+  elif(compChoice==choices[1] and user2==choices[2]):
    user2Count+=1
-  elif(user1==choices[0] and user2==choices[0]):
+  elif(compChoice==choices[0] and user2==choices[0]):
     print("Choices are same ! ")
     continue
-  elif(user1==choices[1] and user2==choices[1]):
+  elif(compChoice==choices[1] and user2==choices[1]):
     print("Choices are same ! ")
     continue
-  elif(user1==choices[2] and user2==choices[2]):
+  elif(compChoice==choices[2] and user2==choices[2]):
     print("Choices are same ! ")
     continue
   if(user1Count>user2Count and user1Count==winNum):
-   print("User1 has won ! ")
-   print("The final score is User1: "+str(winNum)+" and User2: "+str(user2Count)+"")
+   print("computer has won ! ")
+   print("The final score is Computer: "+str(winNum)+" and User2: "+str(user2Count)+"")
    break
   if(user2Count>user1Count and user2Count==winNum):
-   print("The final score is User2: "+str(winNum)+" and User1: "+str(user1Count)+"")
    print("User2 has won ! ")
+   print("The final score is User2: "+str(winNum)+" and Computer: "+str(user1Count)+"")
    break
  
  #ROCK, PAPER, SCISSORS GAME IN PYTHON
