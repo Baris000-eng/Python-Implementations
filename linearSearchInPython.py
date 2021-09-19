2a927e013a857d14f4670f85aac48ec5afb7e187
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-19 15:23:42 +0000

    linear search python

---
+++ linearSearchInPython.py
@@ -0,0 +1,16 @@
+#Linear Search In Python
+
+
+
+
+
+def linear_search(arr, x):
+   for i in range(0,len(arr)):
+      if arr[i] == x:
+         return i
+   return -1
+arr = [1,2,3,4,5,6,7,8,9,10]
+inp= input("Please enter a number that \nyou want to find in the list: ")
+print()
+print()
+print("Number found at index "+str(linear_search(arr,int(inp))))
