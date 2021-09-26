25b7e7f6b5eb4994087e3ccf4649313da66d75fd
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-09-26 20:43:10 +0000

    customized math function

---
+++ customMathFunc.py
@@ -0,0 +1,35 @@
+#Custom Math Functions implemented by me!
+
+
+def customAbsoluteValue(n):
+  if n<0:
+    n=-n
+  return n
+
+def customRoundFunction(n):
+ if((n-n//1)>=0.5):
+    return n//1+1
+ else: 
+    return n//1
+
+def customPowerFunc(num1,num2):
+  a= 1
+  if(num2<0):        
+    num1=1/num1       
+    num2= -num2
+  for i in range(0,num2):
+    a= num1*a
+  return a
+
+print(customAbsoluteValue(-12.292992))
+print(customAbsoluteValue(0))
+print(customAbsoluteValue(-2.4))
+print(customRoundFunction(12.5))
+print(customRoundFunction(12.4))
+print(customRoundFunction(12.6))
+print(customPowerFunc(3,-2))
+print(customPowerFunc(3,2))
+print(customPowerFunc(-3,-2))
+print(customPowerFunc(-3,2))
+
+  
