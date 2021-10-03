7ae91ea6bfc5e89e8ffbe9d07bda32edbde1714a
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2021-10-03 16:29:05 +0000

    wfefew

--- lcm&gcd&count-prime_nums&is_prime&sum_digits.py
+++ lcm&gcd&count-prime_nums&is_prime&sum_digits.py
@@ -42,6 +42,40 @@ def lcm(num1,num2):#A function that finds the least common multiple of the two i
        if j%num2==0 and j%num1==0:
          return j
 
+def is_prime(number):#a function that finds whether a number is prime
+    b=0
+    halfNum=0;
+    remainder=0
+    isPrimeNumber=True
+    halfNum=number//2
+    if(number==0 or number==1):
+        return  not isPrimeNumber;
+    else:
+      for b in range(2,halfNum+1):
+            remainder=number%b;
+            if(remainder==0):
+                isPrimeNumber=False
+    return isPrimeNumber
+
+def count_prime_nums(a,b):#A function that counts the prime numbers from a to b, where a<=b.
+  if a>b:
+    var= a
+    a= b
+    b= var
+
+  prime_count=0
+  for i in range(a,b+1):
+    if is_prime(i):
+      prime_count=prime_count+1
+  return prime_count
+
+
+
+
+
+
+
+
   
 
 print(lcm(0,0))
@@ -58,4 +92,10 @@ print(gcd(100,140))
 print(sum_digits(229))
 print(sum_digits(1234554321))
 print(sum_digits(38109542))
+print(is_prime(91))
+print(is_prime(24))
+print(is_prime(47))
+print(count_prime_nums(13,33))
+print(count_prime_nums(40,21))
+print(count_prime_nums(3,5))
 
