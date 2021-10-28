bee9d410580655c639618a14a45c952199e38f50
Baris000-eng <63741832+Baris000-eng@users.noreply.github.com>
2021-10-28 10:30:46 +0300

    Update customMathFunc.py

--- customMathFunc.py
+++ customMathFunc.py
@@ -1,4 +1,3 @@
-#Custom Math Functions implemented by me!
 def customCeilFunction(j):
   int_part= j//1
   return int_part+1
@@ -20,24 +19,12 @@ def customRoundFunction(n):
  else: 
     return n//1
 
-def customPowerFunc(num1,num2):
-  a= 1
-  if(num2<0):        
-    num1=1/num1       
-    num2= -num2
-  for i in range(0,num2):
-    a= num1*a
-  return a
-
 print(customAbsoluteValue(-12.292992))
 print(customAbsoluteValue(0))
 print(customAbsoluteValue(-2.4))
 print(customRoundFunction(12.5))
 print(customRoundFunction(12.4))
 print(customRoundFunction(12.6))
-print(customPowerFunc(3,-2))
-print(customPowerFunc(3,2))
-print(customPowerFunc(-3,-2))
-print(customPowerFunc(-3,2))
+
 
   
